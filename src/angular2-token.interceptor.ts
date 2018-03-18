b97dec9cd301b477ecad8b76e51ab07d85028e78
Colm <colm.bennett@gmail.com>
2018-03-18 22:33:13 +0000

    start interceptor code

---
+++ src/angular2-token.interceptor.ts
@@ -0,0 +1,27 @@
+import {Injectable} from '@angular/core';
+import {
+    HttpInterceptor, HttpHandler, HttpRequest, HttpEvent, HttpResponse
+}
+    from '@angular/common/http';
+
+import {Observable} from 'rxjs/Observable';
+import {tap} from 'rxjs/operators';
+
+@Injectable()
+export class Angular2TokenInteceptor implements HttpInterceptor {
+    intercept(req: HttpRequest<any>,
+              next: HttpHandler): Observable<HttpEvent<any>> {
+
+        return next.handle(req)
+            .pipe(
+                tap((evt => {
+                        if (evt instanceof HttpResponse) {
+                            console.log('---> status:', evt.status);
+                            console.log('---> filter:', req.params.get('filter'));
+                        }
+                    })
+                )
+            )
+
+    }
+}
